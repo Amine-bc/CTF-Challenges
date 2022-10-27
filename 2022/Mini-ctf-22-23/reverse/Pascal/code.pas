54dafa9b132eb9c02ee584d6736a07d17e0032e6
Mohammed El Amine Bouchoucha <96586941+Amine-bc@users.noreply.github.com>
2022-10-27 21:35:41 +0100

    Update code.pas

--- 2022/Mini-ctf-22-23/reverse/Pascal/code.pas
+++ 2022/Mini-ctf-22-23/reverse/Pascal/code.pas
@@ -1,12 +1,28 @@
 Program hideflag ;
-uses StrUtils;
+uses StrUtils,SysUtils, math ;
 Const key='reLkEgmtWUKJihEdOvHbgKhioPfaqw' ;
+Const password='113417411012050384811774126521235438611205890259952182051125';
 //--------Main--------//
 Var
-flag,flaghidden : String ;
+flag, flaghidden : String ;
+i, num: integer ;
 begin
-WriteLn('Enter the flag:');
+WriteLn('Enter the password:');
 Read(flag);
-flaghidden := XorEncode(key,flag);
-WriteLn(flaghidden);
+flaghidden := '' ;
+// the xor 
+
+for i := 1 to Length(flag) do
+begin
+num := (Ord(flag[i]) xor Ord(key[i])) ;
+flaghidden := flaghidden + InttoStr(num);
+end;
+
+
+// compare the xor result with password //
+if ( Comparestr(password,flaghidden) = 0 ) then 
+begin
+writeln('yes! this is the password, validate the challenge with it');
+end;
+
 end.
