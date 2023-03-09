c6831b1f242d7b5f0ffe35919c7ad9c27df21830
Amine-bc <lm_bouchoucha@esi.dz>
2023-03-10 00:13:45 +0100

    register class, BUffer class ..etc

--- 2023/10DaysCTF_Shellmates/Chall_src/machine.py
+++ 2023/10DaysCTF_Shellmates/Chall_src/machine.py
@@ -1,3 +1,12 @@
+class register():
+    __value = 0
+    def __init__(self):
+        pass   
+    def reg_write(self,data):
+        self.__value = data
+
+
+
 class machine():
     d_seg = 0x0000
     s_seg = 0x0050 #80 in decimal comment has to be deletde at the end
@@ -5,6 +14,10 @@ class machine():
     dataptr = 0 # changes while write in MC
     __MC = [None] * 100
     __name__ = str
+    __r1 = register()
+    __r2 = register()
+    __r3 = register()
+
     def __init__(self,name):
         self.name = name
 
@@ -31,6 +44,40 @@ class machine():
             self.stackptr -= 1
         else:
             print("Stack Overflow")
+
+    def register_read(self,reg):
+        if reg == 1:
+            return self.__r1
+        elif reg == 2:
+            return self.__r2
+        elif reg == 3:
+            return self.__r3
+        else:
+            print("Wrong Register")
+
+
+class Buffer:
+    size = 21
+    __buffer = [None] * size
+    def __init__(self):
+        pass
+    def buffer_load(self,data):
+        self.__buffer=list(data)
+        self.__buffer=self.__buffer[:self.size]
+    def buffer_dump(self):
+        return self.__buffer
+
+
+
+
+
+#flag{cpu_hack_is_fun} #21 char 
+
+
+buf =Buffer()
+buf.buffer_load("flag{cpu_hack_is_fun}")
+print(buf.buffer_dump())
+
 
 
 cpu1 = machine("cpu1_machine")
