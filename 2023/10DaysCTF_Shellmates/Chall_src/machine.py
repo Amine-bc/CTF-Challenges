e2a0f94ea78bf996379bc6e9ba2451695556c6a5
Amine-bc <lm_bouchoucha@esi.dz>
2023-03-10 01:09:00 +0100

    Dataorganization done

--- 2023/10DaysCTF_Shellmates/Chall_src/machine.py
+++ 2023/10DaysCTF_Shellmates/Chall_src/machine.py
@@ -1,32 +1,35 @@
+import json
+
 class register():
     __value = 0
     def __init__(self):
         pass   
     def reg_write(self,data):
         self.__value = data
+    def reg_read(self):
+        return self.__value
 
 
 
 class machine():
     d_seg = 0x0000
     s_seg = 0x0050 #80 in decimal comment has to be deletde at the end
+    e_seg = 0x0051 #81 in decimal comment has to be deletde at the end
     stackptr = 80 # changes while push
-    dataptr = 0 # changes while write in MC
     __MC = [None] * 100
     __name__ = str
     __r1 = register()
     __r2 = register()
     __r3 = register()
-
+    __generalpurpose =[__r1,__r2,__r3]
     def __init__(self,name):
         self.name = name
 
     def MC_write_data(self,addr,data):
-        self.__MC[addr] = data
-        self.__MC[self.stackptr] = data
-        if self.dataptr  < self.stackptr - 1:
-            self.stackptr += 1
+        if addr < self.stackptr or addr > self.s_seg:
+            self.__MC[addr] = data
         else:
+            return None
             print("Data Overflow")
 
     def MC_read(self,addr):
@@ -40,12 +43,12 @@ class machine():
 
     def MC_Stack_push(self,data):
         self.__MC[self.stackptr] = data
-        if self.stackptr > self.dataptr + 1:
+        if self.stackptr > self.d_seg :
             self.stackptr -= 1
         else:
             print("Stack Overflow")
 
-    def register_read(self,reg):
+    def registerslct(self,reg):
         if reg == 1:
             return self.__r1
         elif reg == 2:
@@ -53,7 +56,10 @@ class machine():
         elif reg == 3:
             return self.__r3
         else:
-            print("Wrong Register")
+            return None
+    def getregs(self):
+        return self.__generalpurpose
+        
 
 
 class Buffer:
@@ -73,22 +79,52 @@ class Buffer:
 
 #flag{cpu_hack_is_fun} #21 char 
 
+def machineoperation():
+    cpu1 = machine("cpu1_machine")
+    print(cpu1.name)
+    buf =Buffer()
+    buf.buffer_load("123456789abcdefghijkl")
 
-buf =Buffer()
-buf.buffer_load("flag{cpu_hack_is_fun}")
-print(buf.buffer_dump())
+    datain = buf.buffer_dump()
+    print(datain)
+    dataascii = []
+
+    #for i in datain:
+    #    dataascii.append(hex(ord(i)))
+    
+    print(datain)
+
+    for i in datain:
+        dataascii.append(i)
+
+    print(dataascii)
+
+
+    for i in range(21):
+        if i < 7:
+            cpu1.MC_Stack_push(dataascii[i])
+        elif i < 14:
+            cpu1.MC_write_data(i-7,dataascii[i])
+        elif i < 18:
+            cpu1.MC_write_data(i+67,dataascii[i])
+        else:
+            cpu1.registerslct(21-i).reg_write(dataascii[i])
+
+
+   
+
+
+    
+
+    print(cpu1.MC_dump())
+
+
+    for i in cpu1.getregs():
+        print(i.reg_read())
+
+    #json.dump(cpu1,open("cpu1.json","w"))
 
 
 
-cpu1 = machine("cpu1_machine")
-print(cpu1.name)
 
-cpu1.MC_write_data(0,111)
-cpu1.MC_write_data(1,222)
-cpu1.MC_Stack_push(555)
-cpu1.MC_Stack_push(666)
-cpu1.MC_Stack_push("Amine")
-cpu1.MC_Stack_push(888)
-print(cpu1.MC_dump())
-
-
+machineoperation()
