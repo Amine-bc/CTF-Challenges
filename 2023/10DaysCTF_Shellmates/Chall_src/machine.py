a6ac9db61ca3e9ab55fd5d93814cf6d3dc6ab97d
Amine-bc <lm_bouchoucha@esi.dz>
2023-03-10 01:21:53 +0100

    Basic mem organization done, now the shift idea

--- 2023/10DaysCTF_Shellmates/Chall_src/machine.py
+++ 2023/10DaysCTF_Shellmates/Chall_src/machine.py
@@ -61,7 +61,7 @@ class machine():
         return self.__generalpurpose
     
     def retmachine(self):
-        return {'MC': json.dumps(self.__MC), 'r1': self.__r1.reg_read(), 'r2': self.__r2.reg_read(), 'r3': self.__r3.reg_read() }
+        return {'name': 'flagmachine','MC': json.dumps(self.__MC), 'r1': self.__r1.reg_read(), 'r2': self.__r2.reg_read(), 'r3': self.__r3.reg_read() }
         
 
 
@@ -119,7 +119,7 @@ def machineoperation():
     for i in cpu1.getregs():
         print(i.reg_read())
 
-    json.dump(cpu1.retmachine(),open("cpu1.json","w"))
+    json.dump(cpu1.retmachine(),open("Chall_src/flagmachine.json","w"))
 
 
 
